51b43b86ba2e1f4a970ce3d9d7484033b2737d28
Breithner <breithnerap@gmail.com>
2024-04-30 09:14:53 -0500

    [UPDATE] se modificó el filtrado de las ubicacciones para el empleado

--- employee_stock_location/models/hr_employee.py
+++ employee_stock_location/models/hr_employee.py
@@ -12,13 +12,21 @@ class HrEmployee(models.Model):
 
 
     def _compute_location_ids(self):
-        stock_location = self.env['stock.location']
-        exist_values = stock_location.search([
-            ('name','=', ('%s-%s') %(self.identification_id, self.name)),
-            ('usage', '=', 'transit')])
+        list_transit = []
+        exist_values = []
+        stock_location = self.env['stock.location'].search([('usage', '=', 'transit')])
+        
+        for sl in stock_location:
+            list_transit.append([sl.id,sl.name])
+
+        for lt in list_transit:
+            name_value = (lt[1].replace('-','')).replace(' ', '')
+            if self.identification_id:
+                if ('%s%s') %(((self.identification_id).replace('-', '')).replace(' ', ''),(self.name).replace(' ', '')) == name_value:
+                    exist_values.append(lt[0])
             
         for rec in self:
-            rec.location_ids = exist_values.ids
+            rec.location_ids = exist_values
 
 
     location_ids = fields.Many2many(
