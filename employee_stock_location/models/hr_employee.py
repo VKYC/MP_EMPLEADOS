d6b39c3d3b84e90c21279a567185a28c012315b7
BarucAlvarezDev <nodraruc@outlook.com>
2024-05-27 19:29:02 -0600

    [CHG] Feature transit location on employee

--- employee_stock_location/models/hr_employee.py
+++ employee_stock_location/models/hr_employee.py
@@ -1,34 +1,10 @@
-# -*- coding: utf-8 -*-
-import logging
-
-from odoo import models, fields, api, _
-from odoo.exceptions import UserError, ValidationError
-
-_logger = logging.getLogger(__name__)
+from odoo import models, fields
 
 
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-
-    def _compute_location_ids(self):
-        stock_location = self.env['stock.location'].search([
-            ('employee_id','=', self.id),
-            ('usage','=','transit')])
-
-        for rec in self:
-            rec.location_ids = stock_location.ids
-
-
-
-    location_ids = fields.Many2many(
-        'stock.location',
-        'stock_location_hr_employee_rel',
-        'hr_employee_id',
-        'stock_location_id',
-        compute = _compute_location_ids,
-    )
-
+    location_id = fields.Many2one(comodel_name='stock.location', string='Transit Location')
 
 
 
